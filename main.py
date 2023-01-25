cc911516198bca9b4f625f799c50d56b370b8357
evgenii.karpov <evgenii.karpov@sravni.ru>
2023-01-25 18:40:01 +0300

    Realised pagination

--- main.py
+++ main.py
@@ -4,34 +4,70 @@ import json
 import requests
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from graphene import ObjectType, List, String, Field, JSONString, Schema
+from graphene import (
+    ObjectType, List, String, Int, Connection, Schema, relay
+)
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
 load_dotenv()
 
 
+def get_films(title: str) -> list[dict] and int:
+    """
+    :descr: Make a request to OMDBAPI
+    :param title: Film title to search for
+
+    :return: List of request results and amount of elements in the list
+    """
+    payload = {'apikey': os.getenv('OMDB_API_TOKEN'), 's': title}
+    result = requests.get('http://www.omdbapi.com/', params=payload)
+    film_list = result.json().get('Search')
+
+    return film_list, len(film_list)
+
+
 class Film(ObjectType):
-    Title = String(required=True)
+    """Represents film structure from OMBD API """
+    class Meta:
+        interfaces = (relay.Node, )
+
+    Title = String()
     Year = String()
     imdbID = String()
     Type = String()
     Poster = String()
 
+    @classmethod
+    def resolve_film_title(instance, info):
+        return instance.Title
 
-class FilmResult(ObjectType):
-    Search = List(Film)
-    totalResults = String()
+
+class FilmConnection(Connection):
+    total_count = Int()
+
+    def resolve_total_count(self, info, **kwargs):
+        _, total_amount = get_films(kwargs.get('Title'))
+        return total_amount
+
+    class Meta:
+        node = Film
+
+    class Edge:
+        other = String()
+
+        def resolve_other(instance, info):
+            return "This is other: " + instance.node.other
 
 
 class Query(ObjectType):
+    # Root field due Relay specification
+    node = relay.Node.Field()
+
     film_list = None
-    get_films = Field(FilmResult, Title=String()) #List(Film)
+    get_films = relay.ConnectionField(FilmConnection, Title=String())
 
-    def resolve_get_films(self, info, Title):
-        payload = {'apikey': os.getenv('OMDB_API_TOKEN'), 's': Title}
-        result = requests.get('http://www.omdbapi.com/', params=payload)
-        film_list = result.json()#['Search']
-
+    def resolve_get_films(self, info, **kwargs):
+        film_list, _ = get_films(kwargs.get('Title'))
         return film_list
 
 
